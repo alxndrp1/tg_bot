8d300d7470d6cb8e0b40ee3f393a4478ed1e0573
alxnp <alxnp@yandex.ru>
2022-03-23 21:05:18 +0300

    fix crash add try catch

--- main.py
+++ main.py
@@ -20,14 +20,6 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
 	tg_analytic.statistics(message.chat.id, message.text)
-	if re.fullmatch(r'[7]\d{10}', message.text):
-		markup = types.InlineKeyboardMarkup()
-		btn_my_site = types.InlineKeyboardButton('Открыть чат WhatsApp', 'https://api.whatsapp.com/send?phone='+message.text)
-		markup.add(btn_my_site)
-		bot.send_message(message.chat.id, "Нажмите на кнопку для открытия чата WhatsApp", reply_markup = markup)
-	else:
-		bot.send_message(message.chat.id, "<b>Номер не соответствует необходимому формату. Введите ещё раз!</b>", parse_mode='html')
-		bot.send_message(message.chat.id, "<i>Формат номера: 79XXXXXXXXX</i>", parse_mode='html')
 
 	if message.text[:10] == 'Stasistika':
 		st = message.text.split(' ')
@@ -36,19 +28,49 @@ def send_text(message):
 			with open('%s.txt' %message.chat.id ,'r',encoding='UTF-8') as file:
 				bot.send_document(message.chat.id,file)
 			tg_analytic.remove(message.chat.id)
+			return
 		else:
 			messages = tg_analytic.analysis(st,message.chat.id)
 			bot.send_message(message.chat.id, messages)
+			return
 
 	if message.text[:8] == 'Dda_rekl':
 		str_msg = message.text[8:]
 		df = pd.read_csv('data.csv', delimiter=';', encoding='utf8')
 		for iuser in df['id'].unique():
-			bot.send_message(iuser, str_msg, parse_mode='html')
+			try:
+				bot.send_message(iuser, str_msg, parse_mode='html')
+				return
+			except:
+				return
 
 	if message.text[:8] == 'Dda_test':
 		str_msg = message.text[8:]
-		bot.send_message(message.chat.id, str_msg, parse_mode='html')
+		try:
+			bot.send_message(message.chat.id, str_msg, parse_mode='html')
+			return
+		except:
+			return
 
+	if re.fullmatch(r'[7]\d{10}', message.text[:11]):
+		if message.text[12:16] == 'url':
+			try:
+				bot.send_message(message.chat.id, 'Открыть чат WhatsApp https://api.whatsapp.com/send?phone='+message.text[:11]+' (созданно в боте @whatsapp_chat_by_phone_bot)', parse_mode='html')
+				return
+			except:
+				return
+		markup = types.InlineKeyboardMarkup()
+		btn_my_site = types.InlineKeyboardButton('Открыть чат WhatsApp', 'https://api.whatsapp.com/send?phone='+message.text[:11])
+		markup.add(btn_my_site)
+		try:
+			bot.send_message(message.chat.id, "Нажмите на кнопку для открытия чата WhatsApp (созданно в боте @whatsapp_chat_by_phone_bot)", reply_markup = markup)
+		except:
+			pass
+	else:
+		try:
+			bot.send_message(message.chat.id, "<b>Номер не соответствует необходимому формату. Введите ещё раз!</b>", parse_mode='html')
+			bot.send_message(message.chat.id, "<i>Формат номера: 79XXXXXXXXX</i>", parse_mode='html')
+		except:
+			pass
 
 bot.polling(none_stop=True, interval=1)
